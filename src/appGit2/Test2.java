dbdbf76dfc4bcaf5db713666c4b31709548e8d7c
Rennou <taous.ren@gmail.com>
2019-10-27 14:05:07 -0600

    commit git2 :ajouter 2 lignes ds les deux classes

---
+++ src/appGit2/Test2.java
@@ -0,0 +1,11 @@
+package appGit2;
+
+public class Test2 {
+	
+public void test() {	
+System.out.println("111ccccc111");
+System.out.println("2211+++++112");
+System.out.println("master");
+System.out.println("master2");
+System.out.println("123");}
+}
